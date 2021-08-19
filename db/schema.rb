ce4c1f5aa6c0a47c6ee7b4457b72aa30dbfd88eb
Oleksandr Rudyi <2001abitgor2001@gmail.com>
2021-08-19 13:33:11 +0300

    changed db schema

--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,13 @@ ActiveRecord::Schema.define(version: 2021_08_17_130853) do
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "last_sign_in_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
